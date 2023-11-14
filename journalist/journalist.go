145179db2643e080e1f1eb9188f3b0ebcd6ac5d9
samgozman <sam@gozman.space>
2023-11-14 23:02:10 +0400

    Add comments

--- journalist/journalist.go
+++ journalist/journalist.go
@@ -2,16 +2,19 @@ package journalist
 
 import "context"
 
+// Journalist is the main struct that fetches the news from all providers and merges them into unified list
 type Journalist struct {
 	providers []NewsProvider
 }
 
+// NewJournalist creates a new Journalist instance
 func NewJournalist(providers []NewsProvider) *Journalist {
 	return &Journalist{
 		providers: providers,
 	}
 }
 
+// GetLatestNews fetches the latest news from all providers and merges them into unified list
 func (*Journalist) GetLatestNews(ctx context.Context) ([]News, error) {
 	// TODO: run all providers in parallel and merge the results
 	return nil, nil
