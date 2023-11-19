cec1be020e5655b16f921e0a3839f9cc87fc64f4
samgozman <sam@gozman.space>
2023-11-19 15:37:16 +0400

    Base Composer structure

---
+++ composer/composer.go
@@ -0,0 +1,44 @@
+package composer
+
+import (
+	"context"
+
+	"github.com/samgozman/go-fin-feed/journalist"
+	"github.com/sashabaranov/go-openai"
+)
+
+type Composer struct {
+	OpenAiClient *openai.Client
+}
+
+func NewComposer(openAiClient *openai.Client) *Composer {
+	return &Composer{OpenAiClient: openAiClient}
+}
+
+func (c *Composer) ChooseMostImportantNews(ctx context.Context, news []*journalist.News) []*journalist.News {
+	// TODO: implement (use OpenAiClient)
+	return news
+}
+
+func (c *Composer) ComposeNews(ctx context.Context, news []*journalist.News) []*ComposedNews {
+	// TODO: implement (use OpenAiClient)
+	// Call findNewsMetaData
+	return nil
+}
+
+func (c *Composer) findNewsMetaData(ctx context.Context, news []*journalist.News) []*NewsMeta {
+	// TODO: implement (use OpenAiClient)
+	return nil
+}
+
+type NewsMeta struct {
+	Tickers  []string // tickers mentioned or/and related to the news
+	Markets  []string // US/EU/Asia stocks, bonds, commodities, housing, etc.
+	Hashtags []string // hashtags related to the news (#inflation, #fed, #buybacks, etc.)
+}
+
+type ComposedNews struct {
+	NewsID   string
+	Text     string
+	MetaData *NewsMeta
+}
---
+++ composer/go.mod
@@ -0,0 +1,5 @@
+module github.com/samgozman/go-fin-feed/composer
+
+go 1.21.1
+
+require github.com/sashabaranov/go-openai v1.17.8
