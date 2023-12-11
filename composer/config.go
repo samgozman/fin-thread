d1cef64f98cb47463ac58bec102a1fbdbe88a608
samgozman <sam@gozman.space>
2023-12-11 10:01:11 +0400

    Add hashtags

--- composer/config.go
+++ composer/config.go
@@ -14,7 +14,7 @@ func DefaultConfig() *Config {
 		If news are mentioning some companies and stocks you need to find appropriate stocks 'tickers'. 
 		If news are about some market events you need to fill 'markets' with some index tickers (like SPY, QQQ, or RUT etc.) based on the context.
 		News context can be also related to some popular topics, we call it 'hashtags'.
-		You only need to choose appropriate hashtag (0-3) only from this list: inflation, interestrates, crisis, unemployment, bankruptcy, dividends, IPO, debt, war, buybacks, fed.
+		You only need to choose appropriate hashtag (0-3) only from this list: inflation, interestrates, crisis, unemployment, bankruptcy, dividends, IPO, debt, war, buybacks, fed, AI, crypto, bitcoin.
 		It is OK if you don't find find some tickers, markets or hashtags. It's also possible that you will find none.
 		Next you need to create an informative, original 'text' based on the title and description.
 		You need to write a 'text' that would be easy to read and understand, 1-2 sentences long.
