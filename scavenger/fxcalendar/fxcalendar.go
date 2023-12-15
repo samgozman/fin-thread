3661bee199a5ae7ce55f7aa0457dba4cb5b83ffb
samgozman <sam@gozman.space>
2023-12-15 23:09:00 +0400

    calendar parser

--- scavenger/fxcalendar/fxcalendar.go
+++ scavenger/fxcalendar/fxcalendar.go
@@ -1,25 +1,145 @@
 package fxcalendar
 
-import "time"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/samgozman/fin-thread/utils"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
 
-// ForexCalendar is the struct that fetches the calendar events from the ForexFactory website.
-type ForexCalendar struct {
-	Url        string                  // URL of the calendar page (e.g. https://www.forexfactory.com/calendar)
-	Currencies []ForexCalendarCurrency // Currencies to filter the events by
-	Impacts    []ForexCalendarImpact   // Impacts to filter the events by
+const (
+	ForexCalendarUrl = "https://www.mql5.com/en/economic-calendar/content"
+)
+
+// ForexCalendar is the struct that fetches the calendar events from the mql5.com website.
+// Calendar page: https://www.mql5.com/en/economic-calendar
+type ForexCalendar struct{}
+
+// Fetch fetches the calendar events from the mql5.com website.
+func (c *ForexCalendar) Fetch(ctx context.Context) ([]*ForexCalendarEvent, error) {
+	// importance=9 - high impact
+	// currencies=65743 - CHF, EUR, GBP, JPY, USD, CNY, INR
+	var data = strings.NewReader(`date_mode=1&from=2023-12-11T00%3A00%3A00&to=2023-12-17T23%3A59%3A59&importance=9&currencies=65743`)
+	req, err := http.NewRequest(http.MethodPost, ForexCalendarUrl, data)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	req.Header.Set("accept", "*/*")
+	req.Header.Set("content-type", "application/x-www-form-urlencoded")
+	req.Header.Set("x-requested-with", "XMLHttpRequest")
+	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf("invalid status code error: %d, value %s", res.StatusCode, res.Status))
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("error reading response body: %s", err))
+	}
+	err = res.Body.Close()
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("error closing response body: %s", err))
+	}
+
+	// Unmarshal the response
+	var mql5Events []mql5Calendar
+	if err := json.Unmarshal(body, &mql5Events); err != nil {
+		return nil, errors.New(fmt.Sprintf("error unmarshalling response body: %s", err))
+	}
+
+	var events []*ForexCalendarEvent
+	for _, event := range mql5Events {
+		e, err := parseEvent(&event)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, e)
+	}
+
+	return events, nil
 }
 
-// NewForexCalendar creates a new ForexCalendar instance
-func NewForexCalendar(
-	url string,
-	currencies []ForexCalendarCurrency,
-	impacts []ForexCalendarImpact,
-) *ForexCalendar {
-	return &ForexCalendar{
-		Url:        url,
-		Currencies: currencies,
-		Impacts:    impacts,
+// parseEvent parses a single event from the calendar
+func parseEvent(event *mql5Calendar) (*ForexCalendarEvent, error) {
+	// Parse currency
+	var currency ForexCalendarCurrency
+	switch event.CurrencyCode {
+	case "USD":
+		currency = ForexCalendarCurrencyUSD
+	case "EUR":
+		currency = ForexCalendarCurrencyEUR
+	case "GBP":
+		currency = ForexCalendarCurrencyGBP
+	case "JPY":
+		currency = ForexCalendarCurrencyJPY
+	case "CHF":
+		currency = ForexCalendarCurrencyCHF
+	case "CNY":
+		currency = ForexCalendarCurrencyCNY
+	case "AUD":
+		currency = ForexCalendarCurrencyAUD
+	case "NZD":
+		currency = ForexCalendarCurrencyNZD
+	case "INR":
+		currency = ForexCalendarCurrencyINR
+	default:
+		return nil, errors.New(fmt.Sprintf("unknown currency: %s", event.CurrencyCode))
 	}
+
+	// Parse impact
+	var impact ForexCalendarImpact
+	switch event.Importance {
+	case "low":
+		impact = ForexCalendarImpactLow
+	case "medium":
+		impact = ForexCalendarImpactMedium
+	case "high":
+		impact = ForexCalendarImpactHigh
+	case "none":
+		if event.EventType == 2 {
+			impact = ForexCalendarImpactHoliday
+		} else {
+			impact = ForexCalendarImpactNone
+		}
+	default:
+		return nil, errors.New(fmt.Sprintf("unknown impact: %s", event.Importance))
+	}
+
+	// Parse dates
+	dt, err := utils.ParseDate(event.FullDate)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("error parsing date: %s, value %s", err, event.FullDate))
+	}
+	et, err := utils.ParseDate(event.ReleaseDate)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("error parsing date: %s, value %v", err, event.ReleaseDate))
+	}
+
+	e := &ForexCalendarEvent{
+		DateTime:  dt,
+		EventTime: et,
+		Currency:  currency,
+		Impact:    impact,
+		Title:     event.EventName,
+		Actual:    event.ActualValue,
+		Forecast:  event.ForecastValue,
+		Previous:  event.PreviousValue,
+	}
+
+	return e, nil
 }
 
 // ForexCalendarCurrency impacted currencies(economic markets) by the event
@@ -34,16 +154,18 @@ const (
 	ForexCalendarCurrencyCNY ForexCalendarCurrency = "CNY" // Chinese Yuan
 	ForexCalendarCurrencyAUD ForexCalendarCurrency = "AUD" // Australian Dollar
 	ForexCalendarCurrencyNZD ForexCalendarCurrency = "NZD" // New Zealand Dollar
+	ForexCalendarCurrencyINR ForexCalendarCurrency = "INR" // Indian Rupee
 )
 
 // ForexCalendarImpact impact of the event on the market
 type ForexCalendarImpact = string
 
 const (
-	ForexCalendarImpactLow    ForexCalendarImpact = "Low"    // Low impact event
-	ForexCalendarImpactMedium ForexCalendarImpact = "Medium" // Medium impact event
-	ForexCalendarImpactHigh   ForexCalendarImpact = "High"   // High impact event
-	ForexCalendarImpactNone   ForexCalendarImpact = "None"   // No economic event
+	ForexCalendarImpactLow     ForexCalendarImpact = "Low"      // Low impact event
+	ForexCalendarImpactMedium  ForexCalendarImpact = "Medium"   // Medium impact event
+	ForexCalendarImpactHigh    ForexCalendarImpact = "High"     // High impact event
+	ForexCalendarImpactHoliday ForexCalendarImpact = "Holidays" // Holiday event
+	ForexCalendarImpactNone    ForexCalendarImpact = "None"     // No impact event
 )
 
 type ForexCalendarEvent struct {
@@ -55,4 +177,27 @@ type ForexCalendarEvent struct {
 	Actual    string                // Actual value of the event (if available)
 	Forecast  string                // Forecasted value of the event (if available)
 	Previous  string                // Previous value of the event (if available)
+}
+
+// MQL5 calendar event object
+type mql5Calendar struct {
+	Id               int         `json:"Id"`
+	EventType        int         `json:"EventType"`
+	TimeMode         int         `json:"TimeMode"`
+	Processed        int         `json:"Processed"`
+	Url              string      `json:"Url"`
+	EventName        string      `json:"EventName"`
+	Importance       string      `json:"Importance"`
+	CurrencyCode     string      `json:"CurrencyCode"`
+	ForecastValue    string      `json:"ForecastValue"`
+	PreviousValue    string      `json:"PreviousValue"`
+	OldPreviousValue string      `json:"OldPreviousValue"`
+	ActualValue      string      `json:"ActualValue"`
+	ReleaseDate      int64       `json:"ReleaseDate"`
+	ImpactDirection  int         `json:"ImpactDirection"`
+	ImpactValue      string      `json:"ImpactValue"`
+	ImpactValueF     string      `json:"ImpactValueF"`
+	Country          int         `json:"Country"`
+	CountryName      interface{} `json:"CountryName"`
+	FullDate         string      `json:"FullDate"`
 }
---
+++ scavenger/fxcalendar/fxcalendar_test.go
@@ -0,0 +1,108 @@
+package fxcalendar
+
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func Test_parseEvent(t *testing.T) {
+	tests := []struct {
+		name    string
+		event   *mql5Calendar
+		want    *ForexCalendarEvent
+		wantErr bool
+	}{
+		{
+			name: "case 1 - regular event",
+			event: &mql5Calendar{
+				ActualValue:   "0.2%",
+				CurrencyCode:  "USD",
+				ForecastValue: "0.2%",
+				Importance:    "high",
+				PreviousValue: "0.3%",
+				EventName:     "Core CPI m/m",
+				FullDate:      "2023-11-13T12:58:48",
+				ReleaseDate:   1702450800000,
+			},
+			want: &ForexCalendarEvent{
+				Actual:    "0.2%",
+				Currency:  ForexCalendarCurrencyUSD,
+				DateTime:  time.Date(2023, 11, 13, 12, 58, 48, 0, time.UTC),
+				EventTime: time.Date(2023, 12, 13, 07, 00, 00, 0, time.UTC),
+				Forecast:  "0.2%",
+				Impact:    ForexCalendarImpactHigh,
+				Previous:  "0.3%",
+				Title:     "Core CPI m/m",
+			},
+			wantErr: false,
+		},
+		{
+			name: "case 2 - holiday",
+			event: &mql5Calendar{
+				ActualValue:   "",
+				CurrencyCode:  "EUR",
+				ForecastValue: "",
+				Importance:    "none",
+				EventType:     2,
+				PreviousValue: "",
+				EventName:     "The Day of Flying Spaghetti Monster",
+				FullDate:      "2023-11-13T12:58:48",
+				ReleaseDate:   0,
+			},
+			want: &ForexCalendarEvent{
+				Actual:    "",
+				Currency:  ForexCalendarCurrencyEUR,
+				DateTime:  time.Date(2023, 11, 13, 12, 58, 48, 0, time.UTC),
+				EventTime: time.Time{},
+				Forecast:  "",
+				Impact:    ForexCalendarImpactHoliday,
+				Previous:  "",
+				Title:     "The Day of Flying Spaghetti Monster",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseEvent(tt.event)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseEvent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseEvent() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestForexCalendar_Fetch(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		// Just a stupid test to check if the fetch works and returns something without errors
+		{
+			name:    "fetch",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &ForexCalendar{}
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			got, err := c.Fetch(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Fetch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == 0 {
+				t.Error("Fetch() got 0 len")
+			}
+		})
+	}
+}
