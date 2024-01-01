f11e4107ccd5477630e8f29e1046262a3da81b41
samgozman <sam@gozman.space>
2024-01-01 20:16:01 +0200

    add more for aiJSONStringFixer

--- composer/untils.go
+++ composer/untils.go
@@ -4,10 +4,19 @@ import "regexp"
 
 // Find first array group. This will fix most weird OpenAI bugs with broken JSON
 func aiJSONStringFixer(str string) (string, error) {
-	re := regexp.MustCompile(`\[([\S\s]*)\]`)
+	// Find first array group in the string [{...}]
+	re := regexp.MustCompile(`\[{([\S\s]*)}]`)
 	matches := re.FindString(str)
+	if matches != "" {
+		return matches, nil
+	}
+
+	// If not, try first array []
+	re = regexp.MustCompile(`\[([\S\s]*)]`)
+	matches = re.FindString(str)
 	if matches == "" {
 		return "", newErr(ErrEmptyRegexMatch, "aiJSONStringFixer", "regexp.FindString").WithValue(str)
 	}
+
 	return matches, nil
 }
