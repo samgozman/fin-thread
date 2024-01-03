a7b338fc1f90439ad88c6808f8b087c09fcf273c
samgozman <sam@gozman.space>
2024-01-03 08:36:11 +0200

    fix mistral empty array

--- composer/untils.go
+++ composer/untils.go
@@ -4,6 +4,11 @@ import "regexp"
 
 // Find first array group. This will fix most weird OpenAI bugs with broken JSON
 func aiJSONStringFixer(str string) (string, error) {
+	// Often Mistral bug for empty arrays
+	if str == "[[]]" {
+		return "[]", nil
+	}
+
 	// Find first array group in the string [{...}]
 	re := regexp.MustCompile(`\[{([\S\s]*)}]`)
 	matches := re.FindString(str)
