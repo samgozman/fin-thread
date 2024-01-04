9675b759c16452370339e0f2c1bd521bab73c939
samgozman <sam@gozman.space>
2024-01-04 14:19:09 +0200

    modify prompts

--- composer/prompt.go
+++ composer/prompt.go
@@ -22,8 +22,7 @@ func DefaultPromptConfig() *PromptConfig {
 		It is OK if you don't find find some tickers, markets or hashtags. It's also possible that you will find none.
 		Next you need to create an informative, original 'text' based on the title and description.
 		You need to write a 'text' that would be easy to read and understand, 1-2 sentences long.
-		Always answer in the following JSON format:
-		[{id:"", text:"", tickers:[], markets:[], hashtags:[]}]
+		Always answer in the following JSON format: [{id:"", text:"", tickers:[], markets:[], hashtags:[]}]
 		----------------------------------------
 		ONLY JSON IS ALLOWED as an answer. No explanation or other text is allowed.
 `,
@@ -32,8 +31,7 @@ func DefaultPromptConfig() *PromptConfig {
 				You need to create a short (%v words max) summary for the %v most important financial, 
 				economical, stock market news what happened from the start of the day.
 				Find the main verb in the string and put it into the result JSON.
-				Always answer in the following JSON format:
-				[{summary:"", verb:"", id:"", link:""}]
+				Always answer in the following JSON format: [{summary:"", verb:"", id:"", link:""}]
 				----------------------------------------
 				ONLY JSON IS ALLOWED as an answer. No explanation or other text is allowed.
 `,
