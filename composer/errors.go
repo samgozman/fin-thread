f1cb06f222dfe1017ee0316001c26ca4013d4265
samgozman <sam@gozman.space>
2024-01-07 10:37:12 +0200

    Fix error name

--- composer/errors.go
+++ composer/errors.go
@@ -9,15 +9,15 @@ var (
 	ErrEmptyRegexMatch = errors.New("empty regex match")
 )
 
-// ErrCompose is an error that occurs during news composing process.
-type ErrCompose struct {
+// ComposeError is an error that occurs during news composing process.
+type ComposeError struct {
 	FnName string // Name of the function that caused the error
 	Err    error  // Original error
 	Source string // Source of the error
 	Value  string // Value that caused the error
 }
 
-func (e *ErrCompose) Error() string {
+func (e *ComposeError) Error() string {
 	if e.Value != "" {
 		return fmt.Sprintf("[%s] error in %s: %s (value: %s)", e.FnName, e.Source, e.Err, e.Value)
 	}
@@ -26,14 +26,14 @@ func (e *ErrCompose) Error() string {
 }
 
 // WithValue sets the value that caused the error.
-func (e *ErrCompose) WithValue(value string) *ErrCompose {
+func (e *ComposeError) WithValue(value string) *ComposeError {
 	e.Value = value
 	return e
 }
 
-// newErr creates a new ErrCompose instance with the given error and source.
-func newErr(err error, name, source string) *ErrCompose {
-	return &ErrCompose{
+// newErr creates a new ComposeError instance with the given error and source.
+func newErr(err error, name, source string) *ComposeError {
+	return &ComposeError{
 		FnName: name,
 		Err:    err,
 		Source: source,
--- journalist/errors.go
+++ journalist/errors.go
@@ -2,19 +2,19 @@ package journalist
 
 import "fmt"
 
-// ErrProvider is the error type for the NewsProvider.
-type ErrProvider struct {
+// ProviderError is the error type for the NewsProvider.
+type ProviderError struct {
 	Err          string
 	ProviderName string
 }
 
-func (e *ErrProvider) Error() string {
+func (e *ProviderError) Error() string {
 	return fmt.Sprintf("Provider %s error: %s", e.ProviderName, e.Err)
 }
 
-// newErrProvider creates a new ErrProvider instance with the given provider name and error message.
-func newErrProvider(providerName, err string) *ErrProvider {
-	return &ErrProvider{
+// newErrProvider creates a new ProviderError instance with the given provider name and error message.
+func newErrProvider(providerName, err string) *ProviderError {
+	return &ProviderError{
 		Err:          err,
 		ProviderName: providerName,
 	}
