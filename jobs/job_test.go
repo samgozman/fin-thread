6c37ad312f1474982eac45030361de578c08fb06
samgozman <sam@gozman.space>
2024-01-18 10:18:38 +0400

    test: add tests for prepublishFilter method

--- jobs/job_test.go
+++ jobs/job_test.go
@@ -5,6 +5,8 @@ import (
 	"github.com/google/uuid"
 	"github.com/samgozman/fin-thread/archivist/models"
 	"github.com/samgozman/fin-thread/composer"
+	"github.com/samgozman/fin-thread/scavenger/stocks"
+	"reflect"
 	"testing"
 )
 
@@ -61,6 +63,222 @@ func Test_formatNewsWithComposedMeta(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := formatNewsWithComposedMeta(tt.args.n); got != tt.want {
 				t.Errorf("formatNewsWithComposedMeta() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJob_prepublishFilter(t *testing.T) {
+	type fields struct {
+		stocks  *stocks.StockMap
+		options *JobOptions
+	}
+	type args struct {
+		news []*models.News
+	}
+
+	d1, _ := json.Marshal(composer.ComposedMeta{
+		Tickers: []string{"AAPL"},
+	})
+	d2, _ := json.Marshal(composer.ComposedMeta{
+		Tickers: []string{"PLTR"},
+	})
+	emptyMeta, _ := json.Marshal(composer.ComposedMeta{})
+
+	okID := uuid.New()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []*models.News
+		wantErr bool
+	}{
+		{
+			name: "No filters",
+			fields: fields{
+				stocks:  nil,
+				options: &JobOptions{},
+			},
+			args: args{
+				news: []*models.News{},
+			},
+			want:    []*models.News{},
+			wantErr: false,
+		},
+		{
+			name: "Omit suspicious news",
+			fields: fields{
+				stocks: nil,
+				options: &JobOptions{
+					omitSuspicious: true,
+				},
+			},
+			args: args{
+				news: []*models.News{
+					{
+						ID:           uuid.New(),
+						ComposedText: "Some AAPL news about AAPL stock.",
+						MetaData:     d1,
+						IsSuspicious: true,
+					},
+					{
+						ID:           okID,
+						ComposedText: "Some other AAPL news.",
+						MetaData:     d1,
+						IsSuspicious: false,
+					},
+				},
+			},
+			want: []*models.News{
+				{
+					ID:           okID,
+					ComposedText: "Some other AAPL news.",
+					MetaData:     d1,
+					IsSuspicious: false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Omit news with empty tickers",
+			fields: fields{
+				stocks: nil,
+				options: &JobOptions{
+					omitEmptyMetaKeys: &omitKeyOptions{
+						emptyTickers: true,
+					},
+				},
+			},
+			args: args{
+				news: []*models.News{
+					{
+						ID:           uuid.New(),
+						ComposedText: "Some AAPL news without meta.",
+						MetaData:     emptyMeta,
+						IsSuspicious: false,
+					},
+					{
+						ID:           okID,
+						ComposedText: "Some other AAPL news.",
+						MetaData:     d1,
+						IsSuspicious: false,
+					},
+				},
+			},
+			want: []*models.News{
+				{
+					ID:           okID,
+					ComposedText: "Some other AAPL news.",
+					MetaData:     d1,
+					IsSuspicious: false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Omit unlisted stocks",
+			fields: fields{
+				stocks: &stocks.StockMap{
+					"AAPL": stocks.Stock{},
+				},
+				options: &JobOptions{
+					omitUnlistedStocks: true,
+				},
+			},
+			args: args{
+				news: []*models.News{
+					{
+						ID:           okID,
+						ComposedText: "Some AAPL news without meta.",
+						MetaData:     d1,
+						IsSuspicious: false,
+					},
+					{
+						ID:           uuid.New(),
+						ComposedText: "Some PLTR news.",
+						MetaData:     d2,
+						IsSuspicious: false,
+					},
+				},
+			},
+			want: []*models.News{
+				{
+					ID:           okID,
+					ComposedText: "Some AAPL news without meta.",
+					MetaData:     d1,
+					IsSuspicious: false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Omit if all keys are empty",
+			fields: fields{
+				stocks: nil,
+				options: &JobOptions{
+					omitIfAllKeysEmpty: true,
+				},
+			},
+			args: args{
+				news: []*models.News{
+					{
+						ID:           uuid.New(),
+						ComposedText: "Some AAPL news without meta.",
+						MetaData:     emptyMeta,
+						IsSuspicious: false,
+					},
+					{
+						ID:           okID,
+						ComposedText: "Some other AAPL news.",
+						MetaData:     d1,
+						IsSuspicious: false,
+					},
+				},
+			},
+			want: []*models.News{
+				{
+					ID:           okID,
+					ComposedText: "Some other AAPL news.",
+					MetaData:     d1,
+					IsSuspicious: false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid meta data",
+			fields: fields{
+				stocks:  nil,
+				options: &JobOptions{},
+			},
+			args: args{
+				news: []*models.News{
+					{
+						ID:           uuid.New(),
+						ComposedText: "Some AAPL news without meta.",
+						MetaData:     []byte("invalid"),
+						IsSuspicious: false,
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			job := &Job{
+				stocks:  tt.fields.stocks,
+				options: tt.fields.options,
+			}
+			got, err := job.prepublishFilter(tt.args.news)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("prepublishFilter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("prepublishFilter() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
