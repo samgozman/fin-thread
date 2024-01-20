0f4669e279692f2eac081dba85fe466f3a6437f4
samgozman <sam@gozman.space>
2024-01-20 09:35:10 +0400

    fix: fill sentry error type manually

--- utils/exception.go
+++ utils/exception.go
@@ -1,18 +1,36 @@
 package utils
 
-import "github.com/getsentry/sentry-go"
+import (
+	"errors"
+	"github.com/getsentry/sentry-go"
+	"github.com/mmcdole/gofeed"
+)
 
 // CaptureSentryException is a helper function that captures an exception with the given name and error.
 // The main purpose of this function is to rewrite the exception type to the given name.
 // In Sentry, the exception type is always the name of the error type, which is errors.*something* and is not very useful.
 func CaptureSentryException(name string, hub *sentry.Hub, err error) {
+	errType := errorsLevelMatcher(err)
 	hub.WithScope(func(scope *sentry.Scope) {
 		scope.AddEventProcessor(func(e *sentry.Event, hint *sentry.EventHint) *sentry.Event {
 			// NOTE: we need to change top element type in the stack.
 			// e.Exception[0] is the first element in the stack, so it's the bottom one.
 			e.Exception[len(e.Exception)-1].Type = name
+			e.Level = errType
 			return e
 		})
 		hub.CaptureException(err)
 	})
+}
+
+// errorsLevelMatcher is a helper function that returns the Sentry level for the given error.
+func errorsLevelMatcher(err error) sentry.Level {
+	switch {
+	case errors.Is(err, gofeed.ErrFeedTypeNotDetected):
+		return sentry.LevelWarning
+	case err == nil:
+		return sentry.LevelDebug
+	default:
+		return sentry.LevelError
+	}
 }
