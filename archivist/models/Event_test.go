fa11a9939b818e9435b9a93a469c4fd48180f8cc
samgozman <sam@gozman.space>
2024-01-22 09:45:45 +0400

    chore: remove unnecessary structure

--- archivist/models/Event_test.go
+++ archivist/models/Event_test.go
@@ -46,22 +46,7 @@ func TestEvent_BeforeCreate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &Event{
-				ID:           tt.fields.ID,
-				ChannelID:    tt.fields.ChannelID,
-				ProviderName: tt.fields.ProviderName,
-				Title:        tt.fields.Title,
-				DateTime:     tt.fields.DateTime,
-				Country:      tt.fields.Country,
-				Currency:     tt.fields.Currency,
-				Impact:       tt.fields.Impact,
-				Actual:       tt.fields.Actual,
-				Forecast:     tt.fields.Forecast,
-				Previous:     tt.fields.Previous,
-				CreatedAt:    tt.fields.CreatedAt,
-				UpdatedAt:    tt.fields.UpdatedAt,
-			}
-			if err := e.BeforeCreate(tt.args.in0); (err != nil) != tt.wantErr {
+			if err := tt.fields.BeforeCreate(tt.args.in0); (err != nil) != tt.wantErr {
 				t.Errorf("BeforeCreate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -105,22 +90,7 @@ func TestEvent_BeforeUpdate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &Event{
-				ID:           tt.fields.ID,
-				ChannelID:    tt.fields.ChannelID,
-				ProviderName: tt.fields.ProviderName,
-				Title:        tt.fields.Title,
-				DateTime:     tt.fields.DateTime,
-				Country:      tt.fields.Country,
-				Currency:     tt.fields.Currency,
-				Impact:       tt.fields.Impact,
-				Actual:       tt.fields.Actual,
-				Forecast:     tt.fields.Forecast,
-				Previous:     tt.fields.Previous,
-				CreatedAt:    tt.fields.CreatedAt,
-				UpdatedAt:    tt.fields.UpdatedAt,
-			}
-			if err := e.BeforeUpdate(tt.args.in0); (err != nil) != tt.wantErr {
+			if err := tt.fields.BeforeUpdate(tt.args.in0); (err != nil) != tt.wantErr {
 				t.Errorf("BeforeUpdate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -151,22 +121,7 @@ func TestEvent_ToHeadline(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &Event{
-				ID:           tt.fields.ID,
-				ChannelID:    tt.fields.ChannelID,
-				ProviderName: tt.fields.ProviderName,
-				Title:        tt.fields.Title,
-				DateTime:     tt.fields.DateTime,
-				Country:      tt.fields.Country,
-				Currency:     tt.fields.Currency,
-				Impact:       tt.fields.Impact,
-				Actual:       tt.fields.Actual,
-				Forecast:     tt.fields.Forecast,
-				Previous:     tt.fields.Previous,
-				CreatedAt:    tt.fields.CreatedAt,
-				UpdatedAt:    tt.fields.UpdatedAt,
-			}
-			if got := e.ToHeadline(); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.fields.ToHeadline(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ToHeadline() = %v, want %v", got, tt.want)
 			}
 		})
@@ -200,22 +155,7 @@ func TestEvent_Validate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &Event{
-				ID:           tt.fields.ID,
-				ChannelID:    tt.fields.ChannelID,
-				ProviderName: tt.fields.ProviderName,
-				Title:        tt.fields.Title,
-				DateTime:     tt.fields.DateTime,
-				Country:      tt.fields.Country,
-				Currency:     tt.fields.Currency,
-				Impact:       tt.fields.Impact,
-				Actual:       tt.fields.Actual,
-				Forecast:     tt.fields.Forecast,
-				Previous:     tt.fields.Previous,
-				CreatedAt:    tt.fields.CreatedAt,
-				UpdatedAt:    tt.fields.UpdatedAt,
-			}
-			if err := e.Validate(); (err != nil) != tt.wantErr {
+			if err := tt.fields.Validate(); (err != nil) != tt.wantErr {
 				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
