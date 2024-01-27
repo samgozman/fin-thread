46db887b77abece42384f0427b89ac5080f4e542
samgozman <sam@gozman.space>
2024-01-27 19:23:41 +0400

    feat: add global errors wrapper

---
+++ pkg/errlvl/errlvl.go
@@ -0,0 +1,52 @@
+package errlvl
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Lvl uint8
+
+const (
+	DEBUG Lvl = iota + 1
+	INFO
+	WARN
+	ERROR
+)
+
+// ErrorLevel is a type that represents the severity of an error in the application.
+//
+// This is the global error levels that should be used throughout the application to determine the severity of the error.
+type ErrorLevel error
+
+var (
+	ErrDebug ErrorLevel = errors.New("[DEBUG]") // ErrDebug is returned when the global level is set to DEBUG.
+	ErrInfo  ErrorLevel = errors.New("[INFO]")  // ErrInfo is returned when the global level is set to INFO.
+	ErrWarn  ErrorLevel = errors.New("[WARN]")  // ErrWarn is returned when the global level is set to WARN.
+	ErrError ErrorLevel = errors.New("[ERROR]") // ErrError is returned when the global level is set to ERROR.
+)
+
+// Wrap wraps the given error with the given level.
+func Wrap(err error, level Lvl) error {
+	if hasLevel(err) {
+		return err
+	}
+
+	switch level {
+	case DEBUG:
+		return fmt.Errorf("%w %w", ErrDebug, err)
+	case INFO:
+		return fmt.Errorf("%w %w", ErrInfo, err)
+	case WARN:
+		return fmt.Errorf("%w %w", ErrWarn, err)
+	case ERROR:
+		return fmt.Errorf("%w %w", ErrError, err)
+	default:
+		return fmt.Errorf("%w %w", ErrError, err)
+	}
+}
+
+// hasLevel checks if the given error has a level ser already.
+func hasLevel(err error) bool {
+	return errors.Is(err, ErrDebug) || errors.Is(err, ErrInfo) || errors.Is(err, ErrWarn) || errors.Is(err, ErrError)
+}
---
+++ pkg/errlvl/errlvl_test.go
@@ -0,0 +1,101 @@
+package errlvl
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestWrap(t *testing.T) {
+	type args struct {
+		err   error
+		level Lvl
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantLevel ErrorLevel
+	}{
+		{
+			name: "wrap error with level",
+			args: args{
+				err:   errors.New("test"),
+				level: INFO,
+			},
+			wantLevel: ErrInfo,
+		},
+		{
+			name: "wrap joined errors",
+			args: args{
+				err:   errors.Join(errors.New("test1"), errors.New("test2")),
+				level: WARN,
+			},
+			wantLevel: ErrWarn,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Wrap(tt.args.err, tt.args.level)
+			if !errors.Is(err, tt.wantLevel) {
+				t.Errorf("Wrap() wrong error level = %v, want %v", err, tt.wantLevel)
+			}
+			if !errors.Is(err, tt.args.err) {
+				t.Errorf("Wrap() original error not wrapped = %v, want %v", err, tt.args.err)
+			}
+		})
+	}
+}
+
+func Test_hasLevel(t *testing.T) {
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "error with info level",
+			args: args{
+				err: fmt.Errorf("%w %w", ErrInfo, errors.New("test")),
+			},
+			want: true,
+		},
+		{
+			name: "error with warn level",
+			args: args{
+				err: fmt.Errorf("%w %w", ErrWarn, errors.New("test")),
+			},
+			want: true,
+		},
+		{
+			name: "error with error level",
+			args: args{
+				err: fmt.Errorf("%w %w", ErrError, errors.New("test")),
+			},
+			want: true,
+		},
+		{
+			name: "error with debug level",
+			args: args{
+				err: fmt.Errorf("%w %w", ErrDebug, errors.New("test")),
+			},
+			want: true,
+		},
+		{
+			name: "error without level",
+			args: args{
+				err: errors.New("test"),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasLevel(tt.args.err); got != tt.want {
+				t.Errorf("hasLevel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
